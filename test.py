0e9dc91c74794be89f9aec48c6197b9f32adf5a3
nick125736 <110b26379@mailst.cjcu.edu.tw>
2024-04-10 16:38:34 +0800

    Co-authored-by: 曾敬軒 <110b16908@o365.cjcu.edu.tw>

--- test.py
+++ test.py
@@ -8,17 +8,14 @@ api_key = "API"
 def fetch_youtube_comments(video_id, api_key):
     url = f"https://www.googleapis.com/youtube/v3/commentThreads?part=snippet,replies&videoId={video_id}&key={api_key}"
     
-    # 发送API请求
     response = requests.get(url)
     data = response.json()
     
-    # 提取评论
     comments = []
     for item in data['items']:
         comment = item['snippet']['topLevelComment']['snippet']['textDisplay']
         comments.append(comment)
         
-        # 提取回复
         if 'replies' in item:
             for reply in item['replies']['comments']:
                 reply_text = reply['snippet']['textDisplay']
@@ -26,34 +23,5 @@ def fetch_youtube_comments(video_id, api_key):
     
     return comments
 
-def analyze_sentiment(comment):
-    blob = TextBlob(comment)
-    sentiment_score = blob.sentiment.polarity
-    if sentiment_score > 0:
-        return "Positive"
-    elif sentiment_score < 0:
-        return "Negative"
-    else:
-        return "Neutral"
-
-comments = fetch_youtube_comments(video_id, api_key)
 
 
-for idx, comment in enumerate(comments, start=1):
-    sentiment = analyze_sentiment(comment)
-    print(f"Comment {idx}: {comment} - Sentiment: {sentiment}")
-
-sentiments = {"Positive": 0, "Negative": 0, "Neutral": 0}
-for comment in comments:
-    sentiment = analyze_sentiment(comment)
-    sentiments[sentiment] += 1
-
-# 绘制柱状图
-labels = list(sentiments.keys())
-counts = list(sentiments.values())
-
-plt.bar(labels, counts, color=['green', 'red', 'blue'])
-plt.xlabel('Sentiment')
-plt.ylabel('Count')
-plt.title('Sentiment Analysis of YouTube Comments')
-plt.show()
